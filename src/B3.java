a072c4daafe8de44d3f166b1d0df18b951a19856
neslihany <you@example.com>
2022-12-22 12:13:05 +0100

    v3

---
+++ src/B3.java
@@ -0,0 +1,17 @@
+public class B3 {
+    public static void main(String[] args) {
+
+        /*
+        Projemizde onemli bir degisikilik yapmak istiyoruz
+        bu degisiklige 10 gun calisacagiz
+        Degisikligi yapmaya devam ederken orjinal projemizi
+        riske atmamak ve eger son kullanici kullaniyorsa  olusabilecek sorunlari
+        engellemek icin
+        orjinal proje uzerinde calismak yerine yeni bir branch olusturup
+        o branch de degisiklikleri yapariz.
+
+        calisma sonucunda istenen hedefe ulasabilirsek olusturdugumuz
+        branch i asil branch e kopyalariz
+         */
+    }
+}
