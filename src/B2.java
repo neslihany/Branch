5ff988cb9b168637dc803ebf2e2d067b8ae15e28
neslihany <you@example.com>
2022-12-22 11:53:27 +0100

    branch v2

---
+++ src/B2.java
@@ -0,0 +1,22 @@
+public class B2 {
+    public static void main(String[] args) {
+
+        /*
+        Temelde git ve gitHub icin 3 asamadan bahsedebiliriz.
+        1- Projemiz / klasorumuz ile git i tanistirma
+            git init
+        2- Projemiz / klasorumuz icin version olusturma
+            git add .
+            git commit -m "versiyon adi"
+        3- gitHub ile entegrasyon ve senkronizasyon
+            -ilk defa entegre etmek icin once gitHub da yeni repo olusturup
+            oradaki son uc adimi kopyalayip terminal/cmd ye yapistirip ENTER a bas
+            - ilk entegrasyondan sonra her degisiklik yaptigimizda
+                git add .
+                git commit -m "versiyon adi"
+                git push
+
+
+         */
+    }
+}
